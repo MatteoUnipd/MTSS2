33a5489c544089ac8e0a895844ac9c932a186d98
MatteoWu <matteowu@studenti.unipd.it>
2025-05-09 21:44:09 +0200

    Implementazione Test per App.java

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -12,4 +12,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void checkResult(String input) {
+        // Todo
+    }
 }
