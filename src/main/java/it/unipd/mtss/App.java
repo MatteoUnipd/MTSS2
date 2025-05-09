18c81d82c6bde25db4af9bf3571b8eb750cd0174
MatteoWu <matteowu@studenti.unipd.it>
2025-05-09 20:34:29 +0200

    Implementazione Test per IntegerToRoman.java

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -8,10 +8,8 @@ package it.unipd.mtss;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
